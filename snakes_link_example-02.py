2e9257412d4bc17e4c1a8d2b22b1f29ef8cc071a
Ailton Santos Fh <ailton@Ailtons-MacBook-Air.local>
2021-10-05 12:30:50 +0200

    feat: create new example in a interative way

--- snakes_link_example-02.py
+++ snakes_link_example-02.py
@@ -2,6 +2,10 @@ import snakes.plugins
 snakes.plugins.load("gv", "snakes.nets", "nets")
 from nets import *
 
+##
+## Interative version and using Values in Producer
+##
+
 # producer
 # GET_verb = "verb=GET,"
 producer_verb = "GET"
@@ -14,7 +18,8 @@ producer_uri_key = f"uri={producer_uri}"
 producer_request = f"Request to {producer_uri}"
 # vehicles_response = "uuid"
 producer_response = "username"
-producer_parameter = "username paramter"
+producer_parameter = "username parameter"
+producer_username_parameter = 'Me'
 
 # consumer
 consumer_verb = "GET"
@@ -68,20 +73,65 @@ def factory (cons, prod, init_producer_request=[], init_producer_parameter=[]) :
 # python3 snakes_crAPI-01.py
 def main():
     # create the petri net
-    net, transitions = factory(Variable("username"), Value("repository"), producer_verb_key+producer_uri, "Username01")
-    net.draw("value-0.png")
-    import ipdb; ipdb.set_trace()
+    net = PetriNet("N")
+    transitions = []
 
-    # given the request-response "/2.0/users/{username}"
+    ## 1. we receive the request and the response to /2.0/users/{username}
+    # request to the producer
+    net.add_place(Place(producer_request, producer_verb_key+producer_uri))
+    # parameter to the producer
+    net.add_place(Place(producer_parameter, producer_username_parameter))
+
+    t0 = Transition(producer_uri)
+    transitions.append(t0)
+    net.add_transition(t0)
+
+    # response
+    # username obtained from consumer
+    net.add_place(Place(producer_response, []))
+    # request to the consumer
+    net.add_place(Place(consumer_request, []))
+
+    # connections
+    net.add_input(producer_request, producer_uri, Value(producer_verb_key + producer_uri))
+    net.add_input(producer_parameter, producer_uri, Value(producer_username_parameter))
+    net.add_output(consumer_request, producer_uri, Value(producer_verb_key+consumer_uri))
+    net.add_output(producer_response, producer_uri, Value("Username01"))
+
+    # draw
+    net.draw("value-0.png")
+    import ipdb;
+    ipdb.set_trace()
+
+    # fire transition
     transitions[0].fire(Substitution(username="Username01"))
     net.draw("value-1.png")
 
-    # given the request-response "/2.0/users/{username}"
+    ## 2. we receive the request and the response to /2.0/repositories/{username}
+    # /2.0/repositories/{username}
+    net.add_place(Place(consumer_response, []))
+    t1 = Transition(consumer_uri)
+    net.add_transition(t1)
+    transitions.append(t1)
+
+    # connections
+    net.add_input(consumer_request, consumer_uri, Value(consumer_verb_key + consumer_uri))
+    net.add_input(producer_response, consumer_uri, Variable("username"))
+    net.add_output(consumer_response, consumer_uri, Value("repository01"))
+
+    # draw
+    net.draw("value-2.png")
+    import ipdb;
+    ipdb.set_trace()
+
+    # fire transition
     try:
-        transitions[1].fire(Substitution(username="Username99"))
+        #transitions[1].fire(Substitution(username="Username99"))
+        transitions[1].fire(Substitution(username="Username01"))
     except:
         print("IDOR Attack Detected")
-    net.draw("value-2.png")
+
+    net.draw("value-3.png")
 
 if __name__ == "__main__":
     main()
